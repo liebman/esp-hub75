b91e8d5ac54bc918d677ac9d54c9c4d7254c1d44
liebman <liebman@zod.com>
2024-09-08 10:18:19 -0700

    drop gpio and share Hub75Pins

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,30 @@
 #![feature(type_alias_impl_trait)]
 
 use embedded_graphics::pixelcolor::Rgb888;
+use esp_hal::gpio::ErasedPin;
 
 #[cfg(any(feature = "esp32s3", feature = "esp32c6"))]
 pub mod framebuffer;
-pub mod gpio;
 #[cfg(feature = "esp32s3")]
 pub mod lcd_cam;
 #[cfg(feature = "esp32c6")]
 pub mod parl_io;
 
 pub type Color = Rgb888;
+
+pub struct Hub75Pins {
+    pub red1: ErasedPin,
+    pub grn1: ErasedPin,
+    pub blu1: ErasedPin,
+    pub red2: ErasedPin,
+    pub grn2: ErasedPin,
+    pub blu2: ErasedPin,
+    pub addr0: ErasedPin,
+    pub addr1: ErasedPin,
+    pub addr2: ErasedPin,
+    pub addr3: ErasedPin,
+    pub addr4: ErasedPin,
+    pub blank: ErasedPin,
+    pub clock: ErasedPin,
+    pub latch: ErasedPin,
+}
