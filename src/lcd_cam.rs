ae9817646c7486a4671ce12c242b0e8ed4640ef2
liebman <liebman@zod.com>
2024-12-05 14:44:04 -0800

    WIP - next esp_hal version

--- src/lcd_cam.rs
+++ src/lcd_cam.rs
@@ -1,16 +1,15 @@
 use core::cell::Cell;
 
-use esp_hal::dma::Channel;
-use esp_hal::dma::DmaChannelConvert;
 use esp_hal::dma::DmaDescriptor;
-use esp_hal::dma::DmaEligible;
 use esp_hal::dma::DmaTxBuf;
+use esp_hal::dma::TxChannelFor;
 use esp_hal::gpio::NoPin;
 use esp_hal::lcd_cam::lcd::i8080;
 use esp_hal::lcd_cam::lcd::i8080::Command;
 use esp_hal::lcd_cam::lcd::i8080::TxSixteenBits;
 use esp_hal::lcd_cam::lcd::i8080::I8080;
 use esp_hal::lcd_cam::LcdCam;
+use esp_hal::peripheral::Peripheral;
 use esp_hal::peripherals::LCD_CAM;
 
 use crate::framebuffer::DmaFrameBuffer;
@@ -26,12 +25,12 @@ impl<'d> Hub75<'d, esp_hal::Async> {
     pub fn new_async<CH>(
         lcd_cam: LCD_CAM,
         hub75_pins: Hub75Pins,
-        channel: Channel<'d, esp_hal::Blocking, CH>,
+        channel: impl Peripheral<P = CH> + 'd,
         tx_descriptors: &'static mut [DmaDescriptor],
         frequency: HertzU32,
-    ) -> Self
+    ) -> Result<Self, Hub75Error>
     where
-        CH: DmaChannelConvert<<LCD_CAM as DmaEligible>::Dma>,
+        CH: TxChannelFor<LCD_CAM>,
     {
         let lcd_cam = LcdCam::new(lcd_cam).into_async();
         Self::new_internal(lcd_cam, hub75_pins, channel, tx_descriptors, frequency)
@@ -71,12 +70,12 @@ impl<'d, DM: esp_hal::Mode> Hub75<'d, DM> {
     fn new_internal<CH>(
         lcd_cam: LcdCam<'d, DM>,
         hub75_pins: Hub75Pins,
-        channel: Channel<'d, esp_hal::Blocking, CH>,
+        channel: impl Peripheral<P = CH> + 'd,
         tx_descriptors: &'static mut [DmaDescriptor],
         frequency: HertzU32,
-    ) -> Self
+    ) -> Result<Self, Hub75Error>
     where
-        CH: DmaChannelConvert<<LCD_CAM as DmaEligible>::Dma>,
+        CH: TxChannelFor<LCD_CAM>,
     {
         let (_, blank) = hub75_pins.blank.split();
         let pins = TxSixteenBits::new(
@@ -98,19 +97,22 @@ impl<'d, DM: esp_hal::Mode> Hub75<'d, DM> {
             NoPin,
         );
 
-        let i8080 = I8080::new(
-            lcd_cam.lcd,
-            channel.tx,
-            pins,
-            frequency,
-            i8080::Config::default(),
-        )
-        .with_ctrl_pins(NoPin, hub75_pins.clock);
+        let mut config = esp_hal::lcd_cam::lcd::i8080::Config::default();
+        config.frequency = frequency;
+        let i8080 = I8080::new(lcd_cam.lcd, channel, pins, i8080::Config::default())
+            .map_err(Hub75Error::I8080)?
+            .with_ctrl_pins(NoPin, hub75_pins.clock);
         let i8080 = Cell::new(Some(i8080));
         let tx_descriptors = Cell::new(Some(tx_descriptors));
-        Self {
+        Ok(Self {
             i8080,
             tx_descriptors,
-        }
+        })
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Hub75Error {
+    I8080(esp_hal::lcd_cam::lcd::i8080::ConfigError),
 }
