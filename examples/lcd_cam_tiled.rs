b0e93b1a781567a6a5f74f8d9beb8e7dac6d63a1
liebman <liebman@zod.com>
2025-10-20 16:24:59 -0700

    remove duplicate esp_app_desc!() call

--- examples/lcd_cam_tiled.rs
+++ examples/lcd_cam_tiled.rs
@@ -91,8 +91,6 @@ use heapless::String;
 #[cfg(feature = "log")]
 use log::info;
 
-esp_bootloader_esp_idf::esp_app_desc!();
-
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
