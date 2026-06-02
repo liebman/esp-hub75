f0a58550cd2a7408b69943d97f5f1f874b2c19f7
liebman <liebman@zod.com>
2026-06-02 15:37:16 -0700

    first cut for i2s_parallel using ISR

--- build-all.sh
+++ build-all.sh
@@ -3,17 +3,13 @@ set -e
 
 aliases=(
     build-32
-    build-32-bp
     build-32-l
-    build-32-bl
-    build-32-d
     build-32h
     build-32r
-    # build-c5-li
-    build-c5-bli
-    build-c6r
+    build-c5-bl
     build-c6-bp
     build-c6-bl
+    build-c6r
     build-s3-bp
     build-s3-bl
     build-s3h
